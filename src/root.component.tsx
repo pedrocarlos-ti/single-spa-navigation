f2ec061cd16b7196da4411814d7ff54bb56bfc4b
Pedro Santos <pedrocarlos.ti@gmail.com>
2024-09-24 16:28:01 +0100

    feat: step two update main component

--- src/root.component.tsx
+++ src/root.component.tsx
@@ -1,3 +1,52 @@
+import "./navigation.css";
+import { useState } from "react";
+
 export default function Root(props) {
-  return <section>{props.name} is mounted!</section>;
+  const [showSubMenu, setShowSubMenu] = useState(false);
+
+  const handleMouseEnter = () => {
+    setShowSubMenu(true);
+  };
+
+  const handleMouseLeave = () => {
+    setShowSubMenu(false);
+  };
+
+  return (
+    <nav className="navigation">
+      <ul>
+        <li>
+          <a href="#home">Home</a>
+        </li>
+        <li>
+          <a href="#about">About</a>
+        </li>
+        <li
+          onMouseEnter={handleMouseEnter}
+          onMouseLeave={handleMouseLeave}
+          className="has-submenu"
+        >
+          <a className="has-submenu" href="#services">
+            Services
+          </a>
+          {showSubMenu && (
+            <ul className="submenu">
+              <li>
+                <a href="#investment">Investment</a>
+              </li>
+              <li>
+                <a href="#retirement">Retirement</a>
+              </li>
+              <li>
+                <a href="#savings">Savings</a>
+              </li>
+            </ul>
+          )}
+        </li>
+        <li>
+          <a href="#contact">Contact</a>
+        </li>
+      </ul>
+    </nav>
+  );
 }
